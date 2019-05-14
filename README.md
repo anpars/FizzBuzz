7ddd27db699d58a76fd74128ec006ca58093bc48
aparis <antoine.paris@smile.fr>
2019-05-14 19:45:50 +0200

    Setup fizzbuzz repository

---
+++ README.md
@@ -0,0 +1,2 @@
+A simple FizzBuzz Kata project.
+ 
---
+++ src/test/java/FizzBuzzMainTestShould.java
@@ -0,0 +1,34 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitParamsRunner.class)
+public class FizzBuzzMainTestShould {
+
+    @Test
+    @Parameters({"45", "30", "15"})
+    public void fizzBuzz_on_a(String multiple_of_3_and_5) {
+        assertThat(FizzBuzzMain.doFizzBuzz(multiple_of_3_and_5)).isEqualTo(FizzBuzzMain.FIZZ_BUZZ);
+    }
+
+    @Test
+    @Parameters({"3", "6", "9"})
+    public void fizz_on_a(String multiple_of_3) {
+        assertThat(FizzBuzzMain.doFizzBuzz(multiple_of_3)).isEqualTo(FizzBuzzMain.FIZZ);
+    }
+
+    @Test
+    @Parameters({"25", "5", "10"})
+    public void buzz_on_a(String multiple_of_5) {
+        assertThat(FizzBuzzMain.doFizzBuzz(multiple_of_5)).isEqualTo(FizzBuzzMain.BUZZ);
+    }
+
+    @Test
+    @Parameters({"26", "7", "22"})
+    public void String_on_a(String none_multiple_of_3_or_5) {
+        assertThat(FizzBuzzMain.doFizzBuzz(none_multiple_of_3_or_5)).isEqualTo(FizzBuzzMain.YOU_FAILED_TO_FIND_A_MULTIPLE_OF_3_OR_5);
+    }
+}
